c4c6e302cfc640b02eed677d1ce438f5eedc2a43
Alexandros <alexandros.daglis@epfl.ch>
2015-11-04 18:01:09 +0100

    Added SABRe uBench that differentiates reader from writer threads and binds threads to cores

--- sparc/SABRes/SABRes_son_multithread.c
+++ sparc/SABRes/SABRes_son_multithread.c
@@ -1,6 +1,8 @@
 //Multithreaded SABRe microbenchmark. Allocation of single context (containing the dataset) and a single local buffer
 //(to be partitioned and shared by the threads). Each thread has its own QP.
-//Each thread executes local writes and schedules remote reads (synchronous in this version of the code - for debugging the Flexus model)
+//There are reader and writer threads (number of each is an input parameter).
+//Writers only issue random writes to objects, readers issue SYNCHRONOUS reads to remote memory.
+//Threads are pinned to cores.
 
 #define __STDC_FORMAT_MACROS
 #define _GNU_SOURCE
@@ -10,11 +12,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 #include <sys/mman.h>
 #include "../../libsonuma/sonuma.h"
 #include "../../libsonuma/magic_iface.h"
+#include <sys/pset.h>
 
-#define ITERS 100000
+#define ITERS 100000000
 #define DATA_SIZE 128 //1024	//in bytes
 #define CTX_ID 0
 #define DST_NID 1
@@ -44,19 +48,65 @@ typedef struct data_object {
 
 typedef struct {
 	int id;
+	int is_reader;
 } parm;
 
 uint8_t *lbuff;
 data_object_t *ctxbuff;  
 int iters, num_objects;
-int write_frequency;	//how many reads to have per write
 uint64_t thread_buf_size;
+int readers, writers;
 
 pthread_barrier_t   barrier; 
 
-int par_phase(void *arg) {
+int par_phase_write(void *arg) {
     parm *p=(parm *)arg;
-   
+     
+    int i,j,luckyObj,offset;
+    srand(p->id);		//remove this for lots of conflicts :-)
+    pthread_barrier_wait (&barrier);
+    if (!p->id) call_magic_2_64(1, ALL_SET, 1); //INIT DONE
+     
+    for (i = 0; i<iters; i++) {
+	luckyObj = rand() % num_objects;
+	offset = luckyObj * sizeof(data_object_t);
+  	uint8_t prevLockVal;
+	do {
+		prevLockVal = acquire_lock(&(ctxbuff[luckyObj].lock));	//Test-and-set
+		#ifdef MY_DEBUG
+		if (prevLockVal) {
+			printf("thread %d failed to grab lock of item %d! (lock value = %"PRIu8")\n", p->id, luckyObj, prevLockVal);
+			usleep(10);
+		}
+		#endif
+	} while (prevLockVal);
+	for (j=0; j<DATA_SIZE; j++) {
+		ctxbuff[luckyObj].value[j] ^= 1;
+	}
+	#ifdef MY_DEBUG
+  	printf("version = %" PRIu64 ",\t\
+	  lock = %" PRIu8 ",\t\
+	  key = %" PRIu32 "\n", ctxbuff[luckyObj].version, ctxbuff[luckyObj].lock, ctxbuff[luckyObj].key);
+	#endif
+	ctxbuff[luckyObj].key ^= 7;  //random operation on object
+	for (j=0; j<DATA_SIZE; j+=2) 
+		ctxbuff[luckyObj].value[j] = (uint8_t)i;
+	ctxbuff[luckyObj].version++;
+	ctxbuff[luckyObj].lock = 0;	//unlock
+	call_magic_2_64(luckyObj, OBJECT_WRITE, i);	//signal the completion of a write
+	#ifdef MY_DEBUG
+  	printf("Thread %d: \tprevious lock value = %" PRIu8 ",\t\
+	  	new lock value = %" PRIu8 ",\t\
+		new version = %" PRIu64 "\n", p->id, prevLockVal, ctxbuff[luckyObj].lock, ctxbuff[luckyObj].version);
+	#endif
+    }
+    call_magic_2_64(0, BENCHMARK_END, 0);	//this threads completed its work and it's exiting
+    return 0;
+}
+
+int par_phase_read(void *arg) {
+    parm *p=(parm *)arg;
+    
     rmc_wq_t *wq;
     rmc_cq_t *cq;;
 
@@ -110,67 +160,39 @@ int par_phase(void *arg) {
     uint64_t lbuff_slot, ctx_offset, op_count = 0;
     uint64_t thread_buf_base = lbuff + thread_buf_size * p->id; //this is the local buffer's base address for this thread
     uint32_t wq_head, cq_tail;
-
+     
     for (i = 0; i<iters; i++) {
 	luckyObj = rand() % num_objects;
 	offset = luckyObj * sizeof(data_object_t);
   	uint8_t prevLockVal;
-	//determine whether to read or write
-	if (i % write_frequency == 0) {	//do write
-		do {
-			prevLockVal = acquire_lock(&(ctxbuff[luckyObj].lock));	//Test-and-set
-			#ifdef MY_DEBUG
-			if (prevLockVal) {
-				printf("thread %d failed to grab lock of item %d! (lock value = %"PRIu8")\n", p->id, luckyObj, prevLockVal);
-				usleep(10);
-			}
-			#endif
-		} while (prevLockVal);
-		for (j=0; j<DATA_SIZE; j++) {
-			ctxbuff[luckyObj].value[j] ^= 1;
-		}
-		#ifdef MY_DEBUG
-  		printf("version = %" PRIu64 ",\t\
-		  lock = %" PRIu8 ",\t\
-		  key = %" PRIu32 "\n", ctxbuff[luckyObj].version, ctxbuff[luckyObj].lock, ctxbuff[luckyObj].key);
-		#endif
-		ctxbuff[luckyObj].key ^= 7;  //random operation on object
-		ctxbuff[luckyObj].version++;
-		#ifdef MY_DEBUG
-  		printf("Thread %d: \tprevious lock value = %" PRIu8 ",\t\
-		  	new lock value = %" PRIu8 ",\t\
-			new version = %" PRIu64 "\n", p->id, prevLockVal, ctxbuff[luckyObj].lock, ctxbuff[luckyObj].version);
-		#endif
-		ctxbuff[luckyObj].lock = 0;	//unlock
-	} else {    //schedule rread
-        	lbuff_slot = thread_buf_base + ((op_count * sizeof(data_object_t)) % thread_buf_size);
-       		ctx_offset = luckyObj * sizeof(data_object_t);
-        	wq_head = wq->head;
-        	create_wq_entry(RMC_SABRE, wq->SR, CTX_ID, DST_NID, (uint64_t)lbuff_slot, ctx_offset, sizeof(data_object_t)>>6, (uint64_t)&(wq->q[wq_head]));
-        	call_magic_2_64(wq_head, NEWWQENTRY, op_count);
-		wq->head =  wq->head + 1;
-	        if (wq->head >= MAX_NUM_WQ) {
- 	           	wq->head = 0;
-      	      		wq->SR ^= 1;
-    		}
-        	//sync
-        	cq_tail = cq->tail;
-        	while(cq->q[cq_tail].SR != cq->SR) {}	//wait for request completion (sync mode)
-		if (cq->q[cq_tail].success) {	//No atomicity violation!
-        		call_magic_2_64(cq_tail, SABRE_SUCCESS, op_count);
-		} else {	//Atomicity violation detected
-        		call_magic_2_64(cq_tail, SABRE_ABORT, op_count);
-		}
-        	wq->q[cq->q[cq_tail].tid].valid = 0;	//free WQ entry
-
-        	cq->tail = cq->tail + 1;
-        	if (cq->tail >= MAX_NUM_WQ) {
-            		cq->tail = 0;
-          		cq->SR ^= 1;
-	        }    
-		op_count++;
+        lbuff_slot = thread_buf_base + ((op_count * sizeof(data_object_t)) % thread_buf_size);
+       	ctx_offset = luckyObj * sizeof(data_object_t);
+        wq_head = wq->head;
+        create_wq_entry(RMC_SABRE, wq->SR, CTX_ID, DST_NID, (uint64_t)lbuff_slot, ctx_offset, sizeof(data_object_t)>>6, (uint64_t)&(wq->q[wq_head]));
+        call_magic_2_64(wq_head, NEWWQENTRY, op_count);
+	wq->head =  wq->head + 1;
+	if (wq->head >= MAX_NUM_WQ) {
+ 	    	wq->head = 0;
+      		wq->SR ^= 1;
+    	}
+        //sync
+        cq_tail = cq->tail;
+        while(cq->q[cq_tail].SR != cq->SR) {}	//wait for request completion (sync mode)
+	if (cq->q[cq_tail].success) {	//No atomicity violation!
+        	call_magic_2_64(cq_tail, SABRE_SUCCESS, op_count);
+	} else {	//Atomicity violation detected
+        	call_magic_2_64(cq_tail, SABRE_ABORT, op_count);
 	}
+        wq->q[cq->q[cq_tail].tid].valid = 0;	//free WQ entry
+
+       	cq->tail = cq->tail + 1;
+       	if (cq->tail >= MAX_NUM_WQ) {
+      		cq->tail = 0;
+      		cq->SR ^= 1;
+        }    
+	op_count++;
     }
+    call_magic_2_64(0, BENCHMARK_END, 0);	//this threads completed its work and it's exiting
     free(wq);
     free(cq);
     return 0;
@@ -183,25 +205,22 @@ int main(int argc, char **argv)
 
     int num_iter = (int)ITERS;
     if (argc != 4) {
-        fprintf(stdout,"Usage: %s <num_objects> <num_threads> <write_percentage>\n", argv[0]);
+        fprintf(stdout,"Usage: %s <num_objects> <num_readers> <num_writers>\n", argv[0]);
         return 1;  
     }
     num_objects = atoi(argv[1]);
     uint64_t ctx_size = num_objects*sizeof(data_object_t);
-    num_threads = atoi(argv[2]);
-    uint64_t buf_size = sizeof(data_object_t) * num_threads * MAX_NUM_WQ; 
-    thread_buf_size = buf_size / num_threads;
-    int write_ratio = atoi(argv[3]);	//e.g., 10 means 10%
-    if (write_ratio) {
-    	write_frequency = (100-write_ratio)/write_ratio;	//only works for write ratios <=50%
-    } else { 
-	write_frequency = iters;	//read-only
-    }
-    write_frequency++;
-
+    readers = atoi(argv[2]) ;
+    writers = atoi(argv[3]) ;
+    num_threads = readers+writers;
+    assert(num_threads <= 16);
+    uint64_t buf_size = sizeof(data_object_t) * readers * MAX_NUM_WQ; 
+    if (readers) thread_buf_size = buf_size / readers;
+    else thread_buf_size = 0;
+    
     //local buffer
     lbuff = memalign(PAGE_SIZE, buf_size*sizeof(uint8_t));
-    if (lbuff == NULL) {
+    if (thread_buf_size && lbuff == NULL) {
         fprintf(stdout, "Local buffer could not be allocated. Memalign returned %"PRIu64"\n", 0);
         return 1;
     } 
@@ -244,12 +263,12 @@ int main(int argc, char **argv)
     call_magic_2_64(42, CONTEXT_SIZE, ctx_size);
     fprintf(stdout,"Init done! Allocated %d objects of %d bytes (pure object data = %d, total size = %d bytes).\
 		\nLocal buffer size = %d Bytes.\
-		\nWill now allocate per-thread QPs and run with %d threads.\
-		\nWrite Ratio set to %d\%.\n", 
+		\nWill now allocate per-thread QPs and run with %d reader threads and %d writer threads.\
+		\nEach thread will execute %d ops (reads or writes).\n", 
 		num_objects, sizeof(data_object_t), DATA_SIZE, ctx_size, 
 		buf_size,
-		num_threads, 
-		write_ratio);
+		readers,writers,
+		iters);
 
     //Now prepare the threads
     pthread_t *threads;
@@ -261,11 +280,34 @@ int main(int argc, char **argv)
     pthread_attr_init(&pthread_custom_attr);
     
     p=(parm *)malloc(sizeof(parm)*num_threads);
-
+    int reader_prio[16]={0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15};
+    int rd_idx=0, wr_idx=15, core;
     /* Start up thread */
     for (i=0; i<num_threads; i++) {
 	p[i].id=i;
-	pthread_create(&threads[i], &pthread_custom_attr, par_phase, (void *)(p+i));
+	if (i<readers) {
+		p[i].is_reader = 1;
+		core = reader_prio[rd_idx];
+		rd_idx++;
+		pthread_create(&threads[i], &pthread_custom_attr, par_phase_read, (void *)(p+i));
+		int error = processor_bind(P_LWPID, threads[i], core, NULL);
+		if (error) {
+			printf("Could not bind reader thread %d to core %d! (error %d)\n", i, core, error);
+	      	} else {
+			printf("Bound reader thread %d to core %d\n", i, core);
+       	 	}
+        } else {
+		p[i].is_reader = 0;
+                core = reader_prio[wr_idx];
+		wr_idx--;
+		pthread_create(&threads[i], &pthread_custom_attr, par_phase_write, (void *)(p+i));
+	        int error = processor_bind(P_LWPID, threads[i], core, NULL);
+		if (error) {
+			printf("Could not bind writer thread %d to core %d! (error %d)\n", i, core, error);
+	      	} else {
+			printf("Bound writer thread %d to core %d\n", i, core);
+       	 	}
+        }	
     }
 
     /* Synchronize the completion of each thread. */
